6f0697490d9bbe78a1b4ce10895207e284f29b25
shahvirb <shahvirb@gmail.com>
2025-02-23 09:26:42 -0600

    WIP migrating tandoor

--- stack-transfer/vars.sh
+++ stack-transfer/vars.sh
@@ -3,10 +3,15 @@
 REMOTE_USER="helium"
 REMOTE_HOST="helium.fdatxvault.win"
 
-STACK_NAME="stack-unificontroller"
+STACK_NAME="stack-tandoor"
+DD_DIR="tandoor"
 
 # Associative array mapping remote directories to local destinations
 declare -A DIR_MAP=(
     ["/home/helium/gitsource/helium/${STACK_NAME}"]="/home/shahvirb/gitsource/argon/${STACK_NAME}"
-    ["/home/helium/docker-data/unificontroller"]="/home/shahvirb/docker-data/unificontroller"
+    ["/home/helium/docker-data/${DD_DIR}"]="/home/shahvirb/docker-data/${DD_DIR}"
+    ["/home/helium/gitsource/helium/${STACK_NAME}/ng.tar"]="/home/shahvirb/gitsource/argon/${STACK_NAME}/ng.tar"
+    ["/home/helium/gitsource/helium/${STACK_NAME}/sf.tar"]="/home/shahvirb/gitsource/argon/${STACK_NAME}/sf.tar"
+    # ["/var/lib/docker/volumes/stack-tandoor_nginx_config]="/var/lib/docker/volumes/stack-tandoor_nginx_config/"
+    # ["/var/lib/docker/volumes/stack-tandoor_staticfiles]="/var/lib/docker/volumes/stack-tandoor_staticfiles/"
 )
