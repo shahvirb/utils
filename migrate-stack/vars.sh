2b8c85c5596748200fbe1fdd685657107b357453
shahvirb <shahvirb@gmail.com>
2025-12-30 12:10:58 -0600

    scrutiny migration

--- migrate-stack/vars.sh
+++ migrate-stack/vars.sh
@@ -3,11 +3,14 @@
 REMOTE_USER="helium"
 REMOTE_HOST="helium.fdatxvault.win"
 
-STACK_NAME="stack-vikunja"
-DD_DIR="vikunja"
+STACK_NAME="stack-scrutiny"
+DD_DIR="scrutiny"
 
 # Associative array mapping remote directories to local destinations
+# WARNING: Trailing slashes matter! 
+# - Remote path WITH trailing slash (dir/) copies contents into destination
+# - Remote path WITHOUT trailing slash (dir) copies the directory itself, creating nested folder
 declare -A DIR_MAP=(
-    ["/home/helium/gitsource/helium/${STACK_NAME}"]="/home/shahvirb/gitsource/argon/${STACK_NAME}"
-    ["/home/helium/docker-data/${DD_DIR}/"]="/home/shahvirb/docker-data/${DD_DIR}/"
+    # ["/home/helium/gitsource/helium/${STACK_NAME}/"]="/home/shahvirb/gitsource/argon/${STACK_NAME}"
+    ["/home/helium/docker-data/${DD_DIR}/"]="/home/shahvirb/docker-data/${DD_DIR}"
 )
