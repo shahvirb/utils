84a54d5d2bf8a294bbf447e0330943a506c38b74
shahvirb <shahvirb@gmail.com>
2025-12-30 11:43:23 -0600

    adding usage instructions

--- migrate-stack/copy.sh
+++ migrate-stack/copy.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+# Usage: ./copy.sh
+#
+# Description:
+#   Copies directories from a remote host to local directories using rsync.
+#   The remote-to-local directory mappings are defined in vars.sh (DIR_MAP).
+#
+# Prerequisites:
+#   - vars.sh must be present in the same directory
+#   - SSH access to the remote host configured (REMOTE_USER@REMOTE_HOST)
+#   - rsync installed on both local and remote systems
+#
+# Configuration:
+#   Edit vars.sh to set:
+#   - REMOTE_USER: SSH username for remote host
+#   - REMOTE_HOST: Remote hostname or IP address
+#   - DIR_MAP: Associative array mapping remote directories to local directories
+#
+# Example:
+#   ./copy.sh
+#   (No arguments required - configuration is in vars.sh)
+
 source "$(dirname "$0")/vars.sh"
 
 # Loop through each remote-local pair in the map
@@ -11,8 +32,8 @@ for REMOTE_DIR in "${!DIR_MAP[@]}"; do
     # Create the local directory if it doesn't exist
     mkdir -p "$LOCAL_DIR"
 
-    rsync -av --progress "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
-    # rsync -av --progress -e "ssh -X" --rsync-path="sudo -A rsync" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
+    # rsync -av --progress "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
+    rsync -av --progress -e "ssh -X" --rsync-path="sudo -A rsync" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" "$LOCAL_DIR"
 
     # Check if the copy was successful
     if [ $? -eq 0 ]; then
