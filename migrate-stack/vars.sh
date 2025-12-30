9640c61b9b81e2bf56fcef883a17210ffb4698b2
shahvirb <shahvirb@gmail.com>
2025-12-29 23:53:34 -0600

    vikunja stack migration

--- migrate-stack/vars.sh
+++ migrate-stack/vars.sh
@@ -3,15 +3,11 @@
 REMOTE_USER="helium"
 REMOTE_HOST="helium.fdatxvault.win"
 
-STACK_NAME="stack-tandoor"
-DD_DIR="tandoor"
+STACK_NAME="stack-vikunja"
+DD_DIR="vikunja"
 
 # Associative array mapping remote directories to local destinations
 declare -A DIR_MAP=(
     ["/home/helium/gitsource/helium/${STACK_NAME}"]="/home/shahvirb/gitsource/argon/${STACK_NAME}"
-    ["/home/helium/docker-data/${DD_DIR}"]="/home/shahvirb/docker-data/${DD_DIR}"
-    ["/home/helium/gitsource/helium/${STACK_NAME}/ng.tar"]="/home/shahvirb/gitsource/argon/${STACK_NAME}/ng.tar"
-    ["/home/helium/gitsource/helium/${STACK_NAME}/sf.tar"]="/home/shahvirb/gitsource/argon/${STACK_NAME}/sf.tar"
-    # ["/var/lib/docker/volumes/stack-tandoor_nginx_config]="/var/lib/docker/volumes/stack-tandoor_nginx_config/"
-    # ["/var/lib/docker/volumes/stack-tandoor_staticfiles]="/var/lib/docker/volumes/stack-tandoor_staticfiles/"
+    ["/home/helium/docker-data/${DD_DIR}/"]="/home/shahvirb/docker-data/${DD_DIR}/"
 )
