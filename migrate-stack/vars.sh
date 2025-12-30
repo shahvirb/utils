1b5bb4b748228af8353ef658ba4d72d1d84d56d6
shahvirb <shahvirb@gmail.com>
2025-12-30 14:07:54 -0600

    migrated paperless

--- migrate-stack/vars.sh
+++ migrate-stack/vars.sh
@@ -3,14 +3,14 @@
 REMOTE_USER="helium"
 REMOTE_HOST="helium.fdatxvault.win"
 
-STACK_NAME="stack-scrutiny"
-DD_DIR="scrutiny"
+STACK_NAME="stack-paperless"
+DD_DIR="paperless"
 
 # Associative array mapping remote directories to local destinations
 # WARNING: Trailing slashes matter! 
 # - Remote path WITH trailing slash (dir/) copies contents into destination
 # - Remote path WITHOUT trailing slash (dir) copies the directory itself, creating nested folder
 declare -A DIR_MAP=(
-    # ["/home/helium/gitsource/helium/${STACK_NAME}/"]="/home/shahvirb/gitsource/argon/${STACK_NAME}"
+    ["/home/helium/gitsource/helium/${STACK_NAME}/"]="/home/shahvirb/gitsource/argon/${STACK_NAME}"
     ["/home/helium/docker-data/${DD_DIR}/"]="/home/shahvirb/docker-data/${DD_DIR}"
 )
